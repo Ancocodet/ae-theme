44690af66aaf5d88778e1f80604c1fc460b589d6
Ancozockt Enviroment <support@ancozockt.de>
2022-04-07 16:49:05 +0200

    Adapt styling

--- header.php
+++ header.php
@@ -18,9 +18,8 @@
 	<?php do_action( 'tailpress_header' ); ?>
 
 	<header class="bg-gray-800">
-
-		<div class="mx-auto container">
-			<div class="lg:flex lg:justify-between lg:items-center border-b py-6">
+		<div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+			<div class="relative flex items-center justify-between h-16">
 				<div class="flex justify-between items-center">
 					<div>
 						<?php if ( has_custom_logo() ) { ?>
