7297903c4c6a1cdb349bc1ee955153245c588f6b
Ancozockt Enviroment <support@ancozockt.de>
2022-04-07 17:14:51 +0200

    Adapt footer

--- footer.php
+++ footer.php
@@ -15,6 +15,15 @@
             <div class="mt-2">&copy; <?php echo date_i18n( 'Y' );?> - <?php echo get_bloginfo( 'name' );?></div>
             <div class="md:flex-auto md:flex-row-reverse mt-2 flex-row flex">
                 <?php do_action( 'tailpress_footer' ); ?>
+                <?php
+                wp_nav_menu(
+                    array(
+                        'container_id'    => 'footer-menu',
+                        'li_class'        => 'w-6 mx-5',
+                        'fallback_cb'     => false,
+                    )
+                );
+                ?>
             </div>
         </div>
     </div>
