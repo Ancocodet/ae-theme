6bd3d0fe20c7d56e8be1067bbd172ed90493fca6
Ancocodet <ancoplays@gmail.com>
2022-05-06 09:17:09 +0200

    Update header and footer

--- footer.php
+++ footer.php
@@ -8,26 +8,40 @@
 <?php do_action( 'tailpress_content_after' ); ?>
 
 <footer id="colophon" class="footer relative" role="contentinfo">
-	<?php do_action( 'tailpress_footer' ); ?>
-
-    <div class="bg-gray-800 pt-2">
-        <div class="flex pb-5 px-3 m-auto pt-5 border-t border-gray-700 text-gray-500 text-sm flex-col md:flex-row max-w-6xl">
-            <?php do_action( 'tailpress_footer' ); ?>
-
-            <div class="mt-2">&copy; <?php echo date_i18n( 'Y' );?> - <?php echo get_bloginfo( 'name' );?></div>
-            <?php
-            wp_nav_menu(
-                array(
-                    'menu_class'      => 'md:flex-auto md:flex-row-reverse mt-2 flex-row flex',
-                    'theme_location'  => 'footer',
-                    'li_class'        => 'w-6 mx-5',
-                    'fallback_cb'     => false,
-                )
-            );
-            ?>
+    <?php do_action( 'tailpress_footer' ); ?>
+    <div class="hidden sm:block bg-gray-800">
+        <div class=" max-w-6xl m-auto text-gray-500 flex flex-wrap justify-right">
+            <div class="p-5 w-60">
+                <div class="text-xs uppercase text-gray-500 font-medium">Email us</div>
+                <a class="my-3 block" href="mailto:info@ancozockt.de?subject=Contact&amp;body=Dont´t change the Subject!">info@ancozockt.de<span class="text-teal-600 text-xs p-1">general</span></a>
+                <a class="my-3 block" href="mailto:support@ancozockt.de?subject=Support Request&amp;body=Dont´t change the Subject!">support@ancozockt.de<span class="text-teal-600 text-xs p-1">support</span></a>
+            </div>
+            <div class="p-5 w-48 ">
+                <div class="text-xs uppercase text-gray-500 font-medium">Home</div>
+                <a class="my-3 block" href="/#">Team <span class="text-teal-600 text-xs p-1"></span></a>
+                <a class="my-3 block" href="/#">Partners <span class="text-teal-600 text-xs p-1"></span></a>
+            </div>
+            <div class="p-5 w-48 ">
+                <div class="text-xs uppercase text-gray-500 font-medium">Other</div>
+                <a class="my-3 block" rel="me" href="https://dewp.space/@ancozockt">Mastodon <span class="text-teal-600 text-xs p-1">Social</span></a>
+                <a class="my-3 block" href="https://github.com/Ancocodet">GitHub <span class="text-teal-600 text-xs p-1">Source</span></a>
+            </div>
+            <div class="p-5 w-48 ">
+                <div class="text-xs uppercase text-gray-500 font-medium">Support</div>
+                <a class="my-3 block" href="/#">Help Center <span class="text-teal-600 text-xs p-1"></span></a>
+                <a class="my-3 block" href="/#">Contact Form <span class="text-teal-600 text-xs p-1"></span></a>
+            </div>
         </div>
     </div>
-
+    <div class="bg-gray-800 pt-2">
+        <div class="flex pb-5 px-3 m-auto pt-5 border-t border-gray-700 text-gray-500 text-sm flex-col md:flex-row max-w-6xl">
+            <div class="mt-2">&copy; <?php echo date_i18n( 'Y' );?> Copyright by Ancozockt Environment</div>
+            <div class="md:flex-auto md:flex-row-reverse mt-2 flex-row flex">
+                <a href="https://ancozockt.de/imprint/privacy" class="w-6 mx-5">Privacy</a>
+                <a href="https://ancozockt.de/imprint" class="w-6 mx-5">Imprint</a>
+            </div>
+        </div>
+    </div>
 </footer>
 
 </div>
--- header.php
+++ header.php
@@ -17,56 +17,48 @@
 
 	<?php do_action( 'tailpress_header' ); ?>
 
-	<header class="bg-gray-800">
-		<div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-			<div class="relative flex items-center justify-between h-16">
-				<div class="flex justify-between items-center">
-					<div>
-						<?php if ( has_custom_logo() ) { ?>
-                            <?php the_custom_logo(); ?>
-						<?php } else { ?>
-							<div class="text-white text-lg uppercase">
-								<a href="<?php echo get_bloginfo( 'url' ); ?>" class="font-extrabold text-lg uppercase">
-									<?php echo get_bloginfo( 'name' ); ?>
-								</a>
-							</div>
-
-							<p class="text-sm font-light text-gray-200">
-								<?php echo get_bloginfo( 'description' ); ?>
-							</p>
-
-						<?php } ?>
-					</div>
-
-					<div class="lg:hidden">
-						<a href="#" aria-label="Toggle navigation" id="primary-menu-toggle">
-							<svg viewBox="0 0 20 20" class="inline-block w-6 h-6" version="1.1"
-								 xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-								<g stroke="none" stroke-width="1" fill="currentColor" fill-rule="evenodd">
-									<g id="icon-shape">
-										<path d="M0,3 L20,3 L20,5 L0,5 L0,3 Z M0,9 L20,9 L20,11 L0,11 L0,9 Z M0,15 L20,15 L20,17 L0,17 L0,15 Z"
-											  id="Combined-Shape"></path>
-									</g>
-								</g>
+	<header class="sticky bg-gray-800">
+		<nav x-data="{menu: false}" class="sticky">
+			<div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+				<div class="relative flex items-center justify-between h-16">
+					<div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
+						<button type="button" @click="menu = !menu" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
+							<span class="sr-only">Open main menu</span>
+							<svg x-show="!menu" class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+								<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16"></path>
 							</svg>
-						</a>
+							<svg x-show="menu" class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true" style="display: none;">
+								<path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+							</svg>
+						</button>
+					</div>
+					<div class="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+						<div class="flex-shrink-0 flex items-center">
+							<a href="/en/tailwind">
+								<img class="block lg:hidden h-8 w-auto" src="https://ancozockt.de/img/frontend/logos/logo-icon.svg" alt="Workflow">
+								<img class="hidden lg:block h-8 w-auto" src="https://ancozockt.de/img/frontend/logos/logo.svg">
+							</a>
+						</div>
+						<div class="hidden sm:block sm:ml-6">
+							<div class="flex space-x-4">
+								<a href="https://ancozockt.de/products" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Products</a>
+								<a href="https://ancozockt.de/patchnotes" class="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Patchnotes</a>
+								<a href="https://ancobloggt.de" class="bg-gray-900 text-white hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium">Blog</a>
+							</div>
+						</div>
 					</div>
 				</div>
-
-				<?php
-				wp_nav_menu(
-					array(
-						'container_id'    => 'primary-menu',
-						'container_class' => 'flex space-x-4',
-						'menu_class'      => 'lg:flex lg:-mx-4',
-						'theme_location'  => 'primary',
-						'li_class'        => 'text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium lg:mx-4',
-						'fallback_cb'     => false,
-					)
-				);
-				?>
 			</div>
-		</div>
+
+			<!-- Mobile menu, show/hide based on menu state. -->
+			<div x-show="menu" class="sm:hidden" id="mobile-menu" style="display: none;">
+				<div class="px-2 pt-2 pb-3 space-y-1">
+					<a href="https://ancozockt.de/products" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Products</a>      
+					<a href="https://ancozockt.de/patchnotes" class="text-gray-300 hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Patchnotes</a>
+					<a href="https://ancobloggt.de" class="bg-gray-900 text-white hover:bg-gray-700 hover:text-white block px-3 py-2 rounded-md text-base font-medium">Blog</a>
+				</div>
+			</div>
+		</nav>
 	</header>
 
 	<div id="content" class="site-content flex-grow">
