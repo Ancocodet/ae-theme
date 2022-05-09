797cf1ba64728f8f07d7d04f42e57b98836b1a23
Ancocodet <ancoplays@gmail.com>
2022-05-09 16:10:59 +0200

    Switch to matomo

--- footer.php
+++ footer.php
@@ -44,21 +44,21 @@
     </div>
 </footer>
 
-<!-- Fathom - simple website analytics - https://github.com/usefathom/fathom -->
+<!-- Matomo -->
 <script>
-(function(f, a, t, h, o, m){
-	a[h]=a[h]||function(){
-		(a[h].q=a[h].q||[]).push(arguments)
-	};
-	o=f.createElement('script'),
-	m=f.getElementsByTagName('script')[0];
-	o.async=1; o.src=t; o.id='fathom-script';
-	m.parentNode.insertBefore(o,m)
-})(document, window, '//analytics.ancozockt.de/tracker.js', 'fathom');
-fathom('set', 'siteId', 'PACKJ');
-fathom('trackPageview');
+  var _paq = window._paq = window._paq || [];
+  /* tracker methods like "setCustomDimension" should be called before "trackPageView" */
+  _paq.push(['trackPageView']);
+  _paq.push(['enableLinkTracking']);
+  (function() {
+    var u="//analytics.ancozockt.de/";
+    _paq.push(['setTrackerUrl', u+'matomo.php']);
+    _paq.push(['setSiteId', '3']);
+    var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
+    g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
+  })();
 </script>
-<!-- / Fathom -->
+<!-- End Matomo Code -->
 
 </div>
 
