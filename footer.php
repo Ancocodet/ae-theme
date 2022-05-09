3d665c204b74921e73d3f8a8798ddb8755b854a6
Ancocodet <ancoplays@gmail.com>
2022-05-09 09:24:52 +0200

    Add fathom

--- footer.php
+++ footer.php
@@ -44,6 +44,22 @@
     </div>
 </footer>
 
+<!-- Fathom - simple website analytics - https://github.com/usefathom/fathom -->
+<script>
+(function(f, a, t, h, o, m){
+	a[h]=a[h]||function(){
+		(a[h].q=a[h].q||[]).push(arguments)
+	};
+	o=f.createElement('script'),
+	m=f.getElementsByTagName('script')[0];
+	o.async=1; o.src=t; o.id='fathom-script';
+	m.parentNode.insertBefore(o,m)
+})(document, window, '//analytics.ancozockt.de/tracker.js', 'fathom');
+fathom('set', 'siteId', 'TLMDV');
+fathom('trackPageview');
+</script>
+<!-- / Fathom -->
+
 </div>
 
 <?php wp_footer(); ?>
