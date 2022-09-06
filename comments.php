b2f73857cccc2ab9e98ca5497b0cbdc048a94710
dseydel <dseydel@united-internet.de>
2022-09-06 14:11:24 +0200

    Reformat files

--- comments.php
+++ comments.php
@@ -1,9 +1,4 @@
 <?php
-/**
- * If the current post is protected by a password and
- * the visitor has not yet entered the password we will
- * return early without loading the comments.
- */
 if ( post_password_required() ) {
 	return;
 }
@@ -54,7 +49,7 @@ if ( post_password_required() ) {
 				</div>
 			<?php } ?>
 
-		</nav><!-- #comment-nav-above -->
+		</nav>
 
 	<?php endif; ?>
 
